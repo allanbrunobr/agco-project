4c270e92c35e5d4d61d817c3cecdea7469341237
Allan <allanbruno@gmail.com>
2023-02-27 23:22:47 -0300

    Adicionando domain/

---
+++ src/test/java/com/br/ecore/EcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
